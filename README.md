3c37e06a4266d17fc6a99785d46b437aa49cece9
Ugo Turner <turner.ugo@gmail.com>
2019-01-20 20:51:18 -0500

    My dotfiles

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dotfiles
+My personal dotfiles gathered in one git repository.
+
+``` bash
+bash initrc.sh # Create symlinks for config files
+```
---
+++ initrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sfv $HOME/.dotfiles/vim/ ~/.vim
+ln -sfv ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -sfv ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -sfv ~/.dotfiles/mutt/ ~/.config/mutt/
+
