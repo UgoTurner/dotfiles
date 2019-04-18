505031d9454eaa3cc9e0ae514a4beddd425a060c
Ugo Turner <ugoturner>
2019-04-18 22:14:23 +0200

    Delete Vim bundles

--- initrc.sh
+++ initrc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-ln -sfv $HOME/.dotfiles/vim/ ~/.vim
+ln -sfv ~/.dotfiles/vim/ ~/.vim
 ln -sfv ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -sfv ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -sfv ~/.dotfiles/mutt/ ~/.config/mutt/
 
+cd ~/.dotfiles/vim/bundle/ && git clone https://github.com/VundleVim/Vundle.vim.git
