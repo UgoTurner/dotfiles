ce1b3b71fa9219fb1672d5548b02f6b5996399dd
Ugo Turner <ugoturner>
2019-04-18 22:19:17 +0200

    Update bash folder

--- initrc.sh
+++ initrc.sh
@@ -5,4 +5,5 @@ ln -sfv ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -sfv ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -sfv ~/.dotfiles/mutt/ ~/.config/mutt/
 
-cd ~/.dotfiles/vim/bundle/ && git clone https://github.com/VundleVim/Vundle.vim.git
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.dotfiles/vim/bundle/
+git clone https://github.com/Bash-it/bash-it.git ~/.dotfiles/bash/
